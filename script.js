2e312e9fc581b7a50cc8aa19c07d0d6a8ecefb17
chakradharreddynallu <nalluchakradhar@gmail.com>
2025-01-30 16:39:28 -0500

    Commit 1.0

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function toggleShowMore(button) {
+    const moreText = button.previousElementSibling.querySelector('.more-text');
+    if (moreText.style.display === 'none' || !moreText.style.display) {
+      moreText.style.display = 'inline';
+      button.textContent = 'Show Less';
+    } else {
+      moreText.style.display = 'none';
+      button.textContent = 'Show More';
+    }
+  }
